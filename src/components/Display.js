fc821098ed1a8dbb07f8dd6f60524a95bb43b5bb
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-12-26 00:58:47 -0600

    all test pass

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DrumPad from './DrumPad';
 
 const Display = () => {
+
+    const displayAnimal = (animal) => {
+        return animal;
+    }
+
+    const [currentlyPlaying, setPlaying] = useState(displayAnimal(' '));
+
     return (
-        <div id="display">
-            <DrumPad audioDescription="cow" keyboardKey="Q" audioFile="audio_hero_s-cow-moo.mp3" />
-            <DrumPad audioDescription="goat" keyboardKey="W" audioFile="audio_hero_s-cow-moo.mp3" />
-            <DrumPad audioDescription="cat" keyboardKey="E" />
-            <DrumPad audioDescription="dog" keyboardKey="A" />
-            <DrumPad audioDescription="cow" keyboardKey="S" />
-            <DrumPad audioDescription="pig" keyboardKey="D" />
-            <DrumPad audioDescription="mouse" keyboardKey="Z" />
-            <DrumPad audioDescription="chicken" keyboardKey="X" />
-            <DrumPad audioDescription="duck" keyboardKey="C" />
+        <div id="display">{currentlyPlaying}
+            <DrumPad audioDescription="cow" keyboardKey="Q" audioFile="audio_hero_s-cow-moo.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="goose" keyboardKey="W" audioFile="animals_bird_goose_honk_twice.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="cat" keyboardKey="E" audioFile="animal_cat_meow.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="dog" keyboardKey="A" audioFile="ftus_animals_dog_bark_aggressive_242.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="horse" keyboardKey="S" audioFile="audio_hero_s-horse-whinny.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="pig" keyboardKey="D" audioFile="zapsplat_animals_pig_grunt_snort_002_17838.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="sheep" keyboardKey="Z" audioFile="animals_sheep_bleat_002.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="chicken" keyboardKey="X" audioFile="felix_blume_animals_chicken_cluck_single.mp3" displayAnimal={setPlaying} />
+            <DrumPad audioDescription="duck" keyboardKey="C" audioFile="animal_bird_duck_quack_003.mp3" displayAnimal={setPlaying} />
         </div>
     )
 }
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const DrumPad = props => {
 
     function playSound() {
-        console.log(props.keyboardKey);
         const aud = document.getElementById(props.keyboardKey); 
         aud.play();
+        props.displayAnimal(props.audioDescription);
     }
+
+    const handleKeyPress = evt => {
+        if (evt.key.toUpperCase() === props.keyboardKey) {
+            playSound();
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keypress', handleKeyPress);
+
+        return () => {
+            document.addEventListener('keypress', handleKeyPress);
+        };
+    }, [])
+
     return (
         <div className="drum-pad" id={`${props.audioDescription}`} onClick={playSound}>{props.keyboardKey}<audio className="clip" id={`${props.keyboardKey}`} src={`./media/sounds/${props.audioFile}`} />
         </div>
